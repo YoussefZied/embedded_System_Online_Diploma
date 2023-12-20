715a68e4f34aa82d85d76bfd31afa55d34ff4283
Youssef Mohamed <ym8122000@gmail.com>
2023-12-20 17:55:50 +0200

    4

---
+++ unit2/lesson3/assignment1/product.c
@@ -0,0 +1,16 @@
+/*
+ *
+ *
+ *  Created on: Dec 20, 2023
+ *      Author: youss
+ */
+#include"stdio.h"
+void main()
+{
+ float i=0.0, i2=0.0;
+ printf("Enter two numbers\n");
+ fflush(stdin);
+ fflush(stdout);
+ scanf("%f %f",&i,&i2);
+ printf("Product: %f",i*i2);
+}
