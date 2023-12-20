56ade955c7f88b0d00847841e33186d5492dc2d3
Youssef Mohamed <ym8122000@gmail.com>
2023-12-20 17:00:52 +0200

    3

---
+++ unit2/lesson3/assignment1/sum.c
@@ -0,0 +1,16 @@
+/*
+ * printSentence.c
+ *
+ *  Created on: Dec 20, 2023
+ *      Author: youss
+ */
+#include"stdio.h"
+void main()
+{
+ int i=0, i2=0;
+ printf("Enter two integers\n");
+ fflush(stdin);
+ fflush(stdout);
+ scanf("%d %d",&i,&i2);
+ printf("sum: %d",i+i2);
+}
