beaf123ef693c18623578ae214a0171228b3b068
Youssef Mohamed <ym8122000@gmail.com>
2023-12-20 16:38:00 +0200

    2

---
+++ unit2/lesson3/assignment1/printInt.c
@@ -0,0 +1,16 @@
+/*
+ * printSentence.c
+ *
+ *  Created on: Dec 20, 2023
+ *      Author: youss
+ */
+#include"stdio.h"
+void main()
+{
+ int i=0;
+ printf("Enter an integer\n");
+ fflush(stdin);
+ fflush(stdout);
+ scanf("%d",&i);
+ printf("you have entered %d",i);
+}
