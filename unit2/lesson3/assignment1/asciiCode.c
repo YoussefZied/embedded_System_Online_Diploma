c97035cb224e1bc9fa3deca519ece8c2aeafb284
Youssef Mohamed <ym8122000@gmail.com>
2023-12-24 19:25:21 +0200

    5

---
+++ unit2/lesson3/assignment1/asciiCode.c
@@ -0,0 +1,18 @@
+/*
+ *
+ *
+ *  Created on: Dec 20, 2023
+ *      Author: youss
+ */
+#include"stdio.h"
+void main()
+{
+ char i;
+ printf("Enter a character\n");
+ fflush(stdin);
+ fflush(stdout);
+ scanf("%c ",&i);
+ printf("ascii of %c is %i",i,i);
+ fflush(stdin);
+ fflush(stdout);
+}
