bb6ef5e83d79346ef271b531cdcf65e125d93d71
Youssef Mohamed <ym8122000@gmail.com>
2024-02-13 15:15:02 +0200

    embedded c unit

---
+++ unit3/lesson1/platform_types.h
@@ -0,0 +1,29 @@
+/*
+ * platform_types.h
+ *
+ *  Created on: Feb 13, 2024
+ *      Author: youss
+ */
+#include <stdint.h>
+
+#ifndef PLATFORM_TYPES_H_
+#define PLATFORM_TYPES_H_
+//typedef  boolean
+typedef unsigned char  uint8;
+typedef unsigned short  uint16;
+typedef unsigned int uint32;
+typedef unsigned long long uint64;
+typedef signed char sint8;
+typedef signed short sint16;
+typedef signed int sint32;
+typedef signed long long sint64;
+typedef unsigned char unit8_least;
+typedef unsigned short unit16_least;
+typedef unsigned int unit32_least;
+typedef signed char snit8_least;
+typedef signed short snit16_least
+typedef signed int snit32_least
+//typedef  float32
+//typedef  float64
+typedef void*  VoidPtr;
+typedef const void* ConstVoidPtr
